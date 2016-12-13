7d830338195e8e078e4304f3d108239ef1e0b06b
yuri-swift <yuri.swift.615@gmail.com>
2016-12-13 23:44:42 +0900

    fix about colorFormatter

--- AnimationTabataTimer/ViewController.swift
+++ AnimationTabataTimer/ViewController.swift
@@ -274,8 +274,8 @@ class Formatter {
         
         let hexString:String = hex.trimmingCharacters(in:CharacterSet.whitespacesAndNewlines).uppercased()
         let redString:String = hexString.substring(with: NSRange(location: 0, length: 2))
-        let greenString:String = (hexString as NSString).substring(with: NSRange(location: 2, length: 2))
-        let blueString:String = (hexString as NSString).substring(with: NSRange(location: 4, length: 2))
+        let greenString:String = hexString.substring(with: NSRange(location: 2, length: 2))
+        let blueString:String = hexString.substring(with: NSRange(location: 4, length: 2))
         
         var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
         Scanner(string: rString).scanHexInt32(&r)
